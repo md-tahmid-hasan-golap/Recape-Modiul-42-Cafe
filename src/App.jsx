22cf873b6fce267179573210fefcd8b2aa98341f
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-05-14 21:46:17 +0600

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <Navbar></Navbar>
     </>
   );
 }
